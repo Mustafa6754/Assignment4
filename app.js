f842cffc1ee1fcc481a4dbec3bf4b78bacd40c73
Mustafa6754 <144300820+Mustafa6754@users.noreply.github.com>
2023-09-18 17:56:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+for (let i = 0; i < 10; i++) ;
+
+for (let i = 0; i <= 12; i++)
+  
+
+  for (let count = 0; count < 100; count++);
+
+  for (let i = 3; i > 0; i--) ;
+
+  const myArray = [1, 2, 3, 4, 5];
+
+
+const numberOfElements = myArray.length;
+
+const flag = true;
+
+for (let i = 0; i < pets.length; i++);
+
+  
+  
